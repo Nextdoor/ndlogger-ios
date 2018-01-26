23b5070db4ec5b78d0c846267dd32b90b78334c3
Abhijeet Kumar <abhijeet@nextdoor.com>
2018-01-26 15:18:39 -0800

    Set bundle version on the fly

--- .xcode/generate-xcodeproj.sh
+++ .xcode/generate-xcodeproj.sh
@@ -1,1 +1,6 @@
+#!/bin/bash -e
+
 swift package generate-xcodeproj --xcconfig-overrides xcode.config
+
+PLISTCMD="Set :CFBundleVersion 0.0.1"
+/usr/libexec/PlistBuddy -c "$PLISTCMD" ../Logger.xcodeproj/Logger_Info.plist
